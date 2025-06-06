57ea21fd4e03636178ecc9b8f38279f6747ae215
Yoonkyo <ykk0618@gmail.com>
2025-06-06 15:06:34 -0400

    Modified version

---
+++ install.sh
@@ -0,0 +1,3 @@
+python3 -m pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu116
+python3 -m pip install -r requirements.txt
+python3 -m pip install 'git+https://github.com/Yoonkyo/detectron2.git'
